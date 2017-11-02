bba817bcbea7de7a01528a6fdcd0d1a81b1b74c6
Adam Walter <adam.iverson.walter@gmail.com>
2017-11-02 20:40:00 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -2,7 +2,7 @@ def first_challenge
   contacts = {
     "Jon Snow" => {
       name: "Jon",
-      email: "jon_snow@thewall.we", 
+      email: "jon_snow@thewall.we",
       favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
       knows: nil
     },
@@ -17,11 +17,8 @@ def first_challenge
     data.each do |attribute, value|
       if attribute == :favorite_icecream_flavors
         value.delete_if |flavor| flavor == "strawberry"
-      
+      end  
     end
-    
-    
   end
   contacts
 end
-
